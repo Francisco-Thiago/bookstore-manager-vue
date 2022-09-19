d8125f38639f6bc4f44bb2253fc164fe5460bbc0
Francisco Thiago <vieirathiago779@gmail.com>
2022-09-19 16:43:22 -0300

    first commit

---
+++ bookstore-manager-vue/src/views/AboutView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about">
+    <h1> {{ produtos[0].name }} </h1>
+  </div>
+</template>
+<script>
+import Produto from "../services/products"
+
+export default {
+  data() {
+    return {
+      produtos: []
+    }
+  },
+  mounted() {
+    Produto.listar().then(response => {
+      response.data.content.map(e => {
+        const {name, code, quantity, release} = e;
+        this.produtos.push(e)
+
+        console.log(
+          this.produtos
+          +"nome -> "+ name 
+          +"\ncode -> "+ code 
+          +"\nrelease -> "+ release 
+          +"\nquantity -> "+ quantity
+        );
+      })
+    })
+  }
+}
+</script>
