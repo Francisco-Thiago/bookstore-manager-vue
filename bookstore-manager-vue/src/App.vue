f53109d60646e12697c034721fe601a04f410e33
Francisco Thiago <vieirathiago779@gmail.com>
2022-09-20 16:52:31 -0300

    feat: initial style added

--- bookstore-manager-vue/src/App.vue
+++ bookstore-manager-vue/src/App.vue
@@ -1,35 +1,51 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="pink"
+      flat
+    >
+      <v-container class="py-0 fill-height">
+        <router-link to="/">
+          <v-img
+            max-height="60"
+            max-width="80"
+            src="../src/assets/logo.svg"
+            link
+          ></v-img>
+        </router-link>
+        <v-spacer></v-spacer>
+        <v-btn class="menu-item"
+          v-for="link of links"
+          :key="link.item"
+          :to="link.route"
+          link
+          text
+          >
+          {{ link.item }}
+        </v-btn>
+        <v-spacer></v-spacer>
+      </v-container>
+    </v-app-bar>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
+
+export default {
+  name: 'App',
+  data: () => ({
+  links: [
+      {item: 'Dashboard', route: "/"},
+      {item: 'Editoras', route: "/publishers"},
+      {item: 'Livros', route: "/books"},
+      {item: 'Alugueis', route: "/rentals"},
+      {item: 'Usuarios', route: "/users"}
+  ]
+})
+};
+
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
